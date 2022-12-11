35d80e52dee6db0a33b6ed257b7334b853d897b4
Eliarlan Rodriues <eliarlan.dev@gmail.com>
2022-12-11 18:58:48 -0300

    first commit

---
+++ src/main/java/br/com/eliarlan/edevmoney/EDevmoneyApplication.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.edevmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDevmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDevmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eliarlan/edevmoney/EDevmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.edevmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDevmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
